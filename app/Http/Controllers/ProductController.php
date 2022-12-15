1f1111bb43e90b978c30402f0cc8ad710a0931a9
vasil14 <vasil_v@hotmail.com>
2022-12-15 16:20:42 +0100

    added store function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return response()->json("Skills Index");
+        return response()->json("Products Index");
+    }
+
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => 'required',
+            'color' => 'required'
+        ]);
+
+        Product::create($formFields);
+        return response()->json('Product Created');
     }
 }
